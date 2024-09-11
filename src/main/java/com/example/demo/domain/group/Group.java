0f9beb726c1112e66aa98e65c5b11aba12422d1c
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-09-11 16:28:34 +0200

    Created Group Main for groupfunction

---
+++ src/main/java/com/example/demo/domain/group/Group.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain.group;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "group")
+public class Group {
+}
