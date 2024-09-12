064ada083a5326f1ec2b71c27247f1f48c0bdaa3
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-09-12 08:19:58 +0200

    Created Entity Class

--- src/main/java/com/example/demo/domain/group/Group.java
+++ src/main/java/com/example/demo/domain/group/Group.java
@@ -1,11 +1,14 @@
 package com.example.demo.domain.group;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.userdetails.User;
 
 @Entity
 @Getter
@@ -13,4 +16,32 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "group")
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id")
+    private int group_id;
+
+    @Column(name = "group_motto")
+    @NotNull
+    @Max(20)
+    private String group_motto;
+
+    @Column(name = "group_name")
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String group_name;
+
+    @Column(name = "group_logo_url")
+    @NotNull
+    private String group_logo_url;
+
+    @Column(name = "group_member")
+    @Max(20)
+    private User member;
+
+
+
+
 }
+
